e6d528f63af63c0f99bbae5fa8eef9538b58dff4
alik <qurbonovalijon30@gmail.com>
2024-09-05 17:50:45 +0500

    test6

---
+++ homework20.py
@@ -0,0 +1,79 @@
+# from math import *
+# def jam(a,b):
+#     return a+b
+# def tarh(a,b):
+#     return a-b
+# def zarb(a,b):
+#     return a*b
+# def taksim(a,b):
+#     return a//b
+# def taksimiazdahdor(a,b):
+#     return a/b
+# def bakiyaitaksim(a,b):
+#     return a%b
+# def daraja(a,b):
+#     return a**b
+# def foiz(a,b):
+#     return (a*b)/100
+# def resha(a):
+#     return sqrt(a)
+# while True:
+#     print("Amalro intikhob kuned: ")
+#     print("1. Jam")
+#     print("2. Tarh")
+#     print("3. Zarb")
+#     print("4. Taksim")
+#     print("5. Taksimi azdahdor")
+#     print("6. Bakiya az taksim")
+#     print("7. Daraja")
+#     print("8. Foiz")
+#     print("9. Resha")
+#     print("10. Baromad")
+#     txt=input()
+#     if txt=="1":
+#         print("Enter a collect: ")
+#         a=int(input())
+#         b=int(input())
+#         print(jam(a,b))
+#     if txt=="2":
+#         print("Enter a subtract: ")
+#         a=int(input())
+#         b=int(input())
+#         print(tarh(a,b))
+#     if txt=="3":
+#         print("Enter a multiply: ")
+#         a=int(input())
+#         b=int(input())
+#         print(zarb(a,b))
+#     if txt=="4":
+#         print("Enter a divide: ")
+#         a=int(input())
+#         b=int(input())
+#         print(taksim(a,b))
+#     if txt=="5":
+#         print("Enter a divide: ")
+#         a=int(input())
+#         b=int(input())
+#         print(taksimiazdahdor(a,b))
+#     if txt=="6":
+#         print("Enter a divide: ")
+#         a=int(input())
+#         b=int(input())
+#         print(bakiyaitaksim(a,b))
+#     if txt=="7":
+#         print("Enter a degree: ")
+#         a=int(input())
+#         b=int(input())
+#         print(daraja(a,b))
+#     if txt=="8":
+#         print("Enter a percent: ")
+#         a=int(input())
+#         b=int(input())
+#         print(foiz(a,b))
+#     if txt=="9":
+#         print("Enter a root: ")
+#         a=int(input())
+#         print(resha(a))
+#     if txt=="10":
+#         print("The programm has finished")
+#         break
